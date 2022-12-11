777bd605e795d09d89c627789141129b1ba7566e
JorgePlazaC <chocheplaza@hotmail.com>
2022-12-10 21:56:33 -0300

    Mostrando primeros datos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import Pedido from "./components/pedido";
 
 function App() {
+  //UseState
+  const [cargando, setCargando] = useState(true);
+  const [arrayFacturas, setArrayFacturas] = useState([]);
+
+  //Url usadas
+  const baseUrl = "http://127.0.0.1:8000";
+  const urlFacturas = `${baseUrl}/api/facturas`;
+
+  //UseEfect
+  useEffect(() => {
+    (async () => {
+      await fetchAllAxios();
+    })();
+  }, []);
+
+  //Llamado GET a api
+  const fetchAllAxios = async () => {
+    let api = [urlFacturas];
+    try {
+      await Promise.all(api.map(async (api) => await axios.get(api))).then(
+        async ([{ data: facturas }]) => {
+          setArrayFacturas(await facturas);
+          console.log(await arrayFacturas);
+          setCargando(false)
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   let pedido = {
-    idFactura:1
+    idFactura: 1,
+  };
+
+  const DesplegarPedidos = () =>{
+    if(cargando){
+      return(<h2>Cargando</h2>)
+    }else{
+      return(
+        arrayFacturas.map( factura => (
+          <Pedido
+              key={factura.id}
+              pedido={factura}
+          />
+      ))
+      )
+    }
   }
+  
   return (
     <div>
       <div className="md:w-6/6 xl:w-5/5 bg-gray-800">
@@ -15,9 +64,7 @@ function App() {
           </p>
         </div>
       </div>
-      <Pedido
-        key={pedido.idFactura} 
-        pedido={pedido} />
+      {DesplegarPedidos()}
     </div>
   );
 }
--- src/components/pedido.js
+++ src/components/pedido.js
@@ -2,14 +2,14 @@ import React, { useContext, useRef } from "react";
 
 const Pedido = ({ pedido }) => {
 
-  const { idFactura } = pedido;
+  //const { id } = pedido;
 
   return (
     <div className="w-full px-3 mb-4">
       <div className="p-5 shadow-md bg-white">
         <div className="lg:flex">
           <div className="lg:w-7/12 xl:w-9/12 pl-5">
-            <p className="font-bold text-2xl text-yellow-600 mb-4">{idFactura} </p>
+            <p className="font-bold text-2xl text-yellow-600 mb-4">{pedido.id} </p>
           </div>
         </div>
       </div>
